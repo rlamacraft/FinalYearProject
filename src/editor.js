21d026979a44efaa31a54da0aebe008483c08a3e
Robert <rlamacraft@gmail.com>
2016-11-13 16:26:17 +0000

    Remove unnecessary import

--- src/editor.js
+++ src/editor.js
@@ -1,6 +1,5 @@
 'use strict'
 const Elm = require('./editor_elm.js');
-const parser  = require('./parser.js');
 const {ipcRenderer} = require('electron');
 
 let container = document.getElementById('container');
