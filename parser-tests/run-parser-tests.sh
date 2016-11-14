481e0cd5da01800e6d61625a986c5272c6b0c33d
Robert <rlamacraft@gmail.com>
2016-11-14 15:33:46 +0000

    More parser tests

--- parser-tests/run-parser-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run the Parser test suite
-# To execute the tests, mocha requires that the directory of tests be called "test"
-mv parser-tests test
-../../node_modules/mocha/bin/mocha
-mv test parser-tests
