b39e10cd72697c1e4e27c38867f14e007e6b796c
Robert <rlamacraft@gmail.com>
2016-12-09 15:11:42 +0000

    Web Components Library for V0 spec
    This library allows for Web Components to be defined independent of the
    spec of the Web Components API that they rely on

---
+++ src/WebComponentsLibrarySpecV0.js
@@ -0,0 +1,41 @@
+/*
+* This library allow for Web Components, both presentation components and other, to be defined
+* in a spec agnostic manner so that when V1 spec is fully supported this library may be replaced
+* with a version targeted at V1 of the spec rather than V0 as this is.
+*/
+
+
+/*
+* This will register a component. In this implementation the Class will
+* be decomposed, by V1 of the spec will use the Class directly in the definition.
+* 'Class' must be a class defintion, that extends HTML Element (see above defintion),
+* as well as provide a getter for the template.
+*/
+const RegisterComponent = function(commandName, functions) {
+  const proto = Object.create(HTMLElement.prototype);
+
+  proto.createdCallback = function() {
+    const template = document.getElementById(`${commandName}_import`).import.getElementById(`${commandName}_template`);
+    const root = this.createShadowRoot();
+    const clone = document.importNode(template.content, true);
+    root.appendChild(clone);
+    if(typeof(functions.onCreated) != "undefined") {
+      functions.onCreated(this);
+    }
+    correctSlot(root);
+  }
+
+  document.registerElement(`pres-${commandName}`, {
+    prototype: proto
+  });
+}
+
+/* Corrects the slot used in the HTML template to fit v1 with a content tag that fits v0 */
+function correctSlot(component) {
+  const slot = component.querySelector('slot[name="content"]');
+  const contentSlot = document.createElement("content");
+  slot.appendChild(contentSlot);
+}
+
+/* This does nothing and is just a placeholder */
+function setup(commandName, element) {}
--- src/components/Component_Lib.js
+++
@@ -1,4 +0,0 @@
-const attachShadowDOM = function(thisThing, template) {
-  const shadowRoot = thisThing.attachShadow({mode : 'open'});
-  shadowRoot.appendChild(template.content.cloneNode(true));
-}
