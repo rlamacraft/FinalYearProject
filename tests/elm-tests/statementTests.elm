a97b96442eadba93e827de5c215104249d16c765
Robert <rlamacraft@gmail.com>
2017-02-10 12:28:54 +0000

    tests for numOfLeafCommandDescendents

--- tests/elm-tests/statementTests.elm
+++ tests/elm-tests/statementTests.elm
@@ -8,6 +8,7 @@ all : Test
 all =
   describe "All Statement Tests"
     [ isRenderableCommand
+    , numOfLeafCommandDescendents
     ]
 
 isRenderableCommand : Test
@@ -30,4 +31,41 @@ isRenderableCommand =
         \() ->
           Statement.isRenderableCommand genericCommand
             |> Expect.equal True
+      ]
+
+numOfLeafCommandDescendents : Test
+numOfLeafCommandDescendents =
+  let
+    stringStatement       = Statement.StringStatement "foo"
+    hiddenCommand         = Statement.Command "hidden" [Statement.StringStatement "hidden message"] "hidden message"
+    noCommandDescendents  = Statement.Command "leaf" [stringStatement] "foo"
+    oneCommandDescendent  = Statement.Command "parent" [noCommandDescendents] "\\leaf{foo}"
+    twoCommandDescendents = Statement.Command "parent" [noCommandDescendents, noCommandDescendents] "\\leaf{foo}\n\\leaf{foo}"
+    grandparentOfLeaf     = Statement.Command "grandparent" [oneCommandDescendent] "\\parent{\\leaf{foo}}"
+  in
+    describe "Tests for numOfLeafCommandDescendents"
+      [ test "String Statement" <|
+        \() ->
+          Statement.numOfLeafCommandDescendents stringStatement
+            |> Expect.equal 0
+      , test "Hidden Statement" <|
+        \() ->
+          Statement.numOfLeafCommandDescendents hiddenCommand
+            |> Expect.equal 0
+      , test "No Descendents that are Commands" <|
+        \() ->
+          Statement.numOfLeafCommandDescendents noCommandDescendents
+            |> Expect.equal 1
+      , test "One Descendent that is a Command" <|
+        \() ->
+          Statement.numOfLeafCommandDescendents oneCommandDescendent
+            |> Expect.equal 1
+      , test "Two Descendent that is a Command" <|
+        \() ->
+          Statement.numOfLeafCommandDescendents twoCommandDescendents
+            |> Expect.equal 2
+      , test "Parent of a Command that has One Descendent that is a Command" <|
+        \() ->
+          Statement.numOfLeafCommandDescendents grandparentOfLeaf
+            |> Expect.equal 1
       ]
