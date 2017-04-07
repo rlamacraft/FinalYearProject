76102d95bf05c5be87679d159df86f997cff4929
Robert <rlamacraft@gmail.com>
2017-04-07 15:05:57 +0100

    Remove pointless cd commands from test script

--- src/tests/run-tests.sh
+++ src/tests/run-tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Run all the test suites
-cd tests
 echo -e "\033[1;36mRunning Elm Tests\033[0m"
 ./elm-tests/run-elm-tests.sh
 echo -e "\033[1;36mRunning Parser Tests\033[0m"
 ./parser-tests/run-parser-tests.sh
-cd ..
